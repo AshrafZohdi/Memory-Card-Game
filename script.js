ac5d059c60166fd9e44c97e1a96144ff298eeb75
Mohammad Ashraf <mohammadashraf2001@gmail.com>
2023-10-10 16:41:29 +0100

    New features
    Added a timer and flip counter, i also added a relaxed mode and a challenge mode, however when switching to challenge mode mid game the timer doesnt reset. Im working on fixing that

--- script.js
+++ script.js
@@ -1,8 +1,73 @@
 const cards = document.querySelectorAll(".card");
 
+let gameMode = "relaxed";
 let matched = 0;
 let cardOne, cardTwo;
 let disableDeck = false;
+let timerInterval;
+let elapsedTime = 0;
+let countdownTime = 120;
+let flipCount = 0;
+
+// Function to update the timer display
+function updateTimer() {
+  const timerElement = document.getElementById("timer");
+
+  if (gameMode === "challenge") {
+    if (countdownTime <= 0) {
+      // Timer reached zero, end the game in challenge mode
+      clearInterval(timerInterval);
+      disableDeck = true; // Disable further card clicks
+      setTimeout(() => {
+        // Display a game-over message
+        alert("Game Over - Challenge Mode");
+      }, 1000); // Wait for 1 second before showing the message
+    } else {
+      const minutes = Math.floor(countdownTime / 60);
+      const seconds = countdownTime % 60;
+      const formattedTime = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+      timerElement.textContent = formattedTime;
+      countdownTime--; // Decrease the countdown time
+
+      // Check if the player has matched all cards (beat the challenge)
+      if (matched == 8) {
+        clearInterval(timerInterval); // Stop the timer
+        // Display a message or perform any other actions for completing the challenge
+        alert("Challenge Completed! Timer Reset.");
+        resetGame(); // Reset the game, including the timer
+      }
+    }
+  } else if (gameMode === "relaxed") {
+    const minutes = Math.floor(elapsedTime / 60);
+    const seconds = elapsedTime % 60;
+    const formattedTime = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+    timerElement.textContent = formattedTime;
+    elapsedTime++; // Increase elapsed time
+  }
+  /*
+  elapsedTime++;
+  const minutes = Math.floor(elapsedTime / 60);
+  const seconds = elapsedTime % 60;
+  const formattedTime = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  timerElement.textContent = formattedTime;
+*/
+}
+
+// Function to increment the flip counter
+function incrementFlipCounter() {
+  flipCount++;
+  const flipCounterElement = document.getElementById("flip-count");
+  flipCounterElement.textContent = flipCount;
+}
+
+// Add event listeners to cards to count flips and start the timer
+cards.forEach((card) => {
+  card.addEventListener("click", function () {
+    if (!disableDeck && !this.classList.contains("flip")) {
+      incrementFlipCounter();
+    }
+  });
+});
 
 function flipCard({ target: clickedCard }) {
   if (cardOne !== clickedCard && !disableDeck) {
@@ -15,6 +80,12 @@ function flipCard({ target: clickedCard }) {
     let cardOneImg = cardOne.querySelector(".back-view img").src,
       cardTwoImg = cardTwo.querySelector(".back-view img").src;
     matchCards(cardOneImg, cardTwoImg);
+    // Start the timer based on the selected game mode
+    if (gameMode === "challenge" && !timerInterval) {
+      timerInterval = setInterval(updateTimer, 1000);
+    } else if (gameMode === "relaxed" && elapsedTime === 0) {
+      timerInterval = setInterval(updateTimer, 1000);
+    }
   }
 }
 
@@ -23,7 +94,7 @@ function matchCards(img1, img2) {
     matched++;
     if (matched == 8) {
       setTimeout(() => {
-        return shuffleCard();
+        return resetGame();
       }, 1000);
     }
     cardOne.removeEventListener("click", flipCard);
@@ -57,6 +128,48 @@ function shuffleCard() {
     card.addEventListener("click", flipCard);
   });
 }
+
+function resetGame() {
+  // Stop the timer interval and reset elapsed time
+  clearInterval(timerInterval);
+  elapsedTime = 0;
+
+  // Reset flip counter
+  flipCount = 0;
+  const flipCounterElement = document.getElementById("flip-count");
+  flipCounterElement.textContent = flipCount;
+
+  // Reset the matched count
+  matched = 0;
+
+  // Reset the countdown time for challenge mode
+  if (gameMode === "challenge") {
+    countdownTime = 120; // Reset the countdown time to 2 minutes (or your desired initial time)
+  }
+
+  // Shuffle the cards and reset their state
+  shuffleCard();
+}
+
+const relaxedModeRadio = document.getElementById("relaxed-mode");
+const challengeModeRadio = document.getElementById("challenge-mode");
+
+relaxedModeRadio.addEventListener("change", function () {
+  if (gameMode !== "relaxed") {
+    gameMode = "relaxed";
+    resetGame();
+  }
+});
+challengeModeRadio.addEventListener("change", function () {
+  if (gameMode !== "challenge") {
+    gameMode = "challenge";
+    resetGame();
+  }
+});
+
+// Add a click event listener to the reset button
+const resetButton = document.getElementById("reset-button");
+resetButton.addEventListener("click", resetGame);
 
 shuffleCard();
 
